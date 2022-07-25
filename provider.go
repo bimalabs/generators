501b09d8469a2509cceadb0c37130e2a76d4b165
Muhammad Surya Ikhsanuddin <surya.kejawen@gmail.com>
2022-07-25 17:14:38 +0700

    fix generator

--- provider.go
+++ provider.go
@@ -12,7 +12,7 @@ const ModuleRegister = "@modules:register"
 type Provider struct {
 }
 
-func (p *Provider) Generate(template *Template, modulePath string, packagePath string, templatePath string) {
+func (p *Provider) Generate(template *Template, modulePath string, driver string) {
 	workDir, _ := os.Getwd()
 
 	var path strings.Builder
